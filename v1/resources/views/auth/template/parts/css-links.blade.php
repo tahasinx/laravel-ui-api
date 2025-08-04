fbb0f03e7cd04bf92e084f42d774be66e6afea3b
anazititís gnósis <devjr.tryus@gmail.com>
2025-08-04 14:22:18 +0600

    update 20250804 0222PM

--- v1/resources/views/auth/template/parts/css-links.blade.php
+++ v1/resources/views/auth/template/parts/css-links.blade.php
@@ -6,16 +6,3 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('auth/css/clock.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('auth/css/style.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('auth/css/root.css') }}">
-
-{{-- <script>
-    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-    const isDarkMode = darkModeMediaQuery.matches;
-
-    const faviconx = document.querySelector('link[rel="icon"]');
-
-    if (isDarkMode) {
-        faviconx.href = '{{ asset('app/root/logo/icon-white.png') }}';
-    } else {
-        faviconx.href = '{{ asset('app/root/logo/icon-dark.png') }}';
-    }
-</script> --}}
